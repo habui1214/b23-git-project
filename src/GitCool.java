bb831a93ae6b5afd71716d5562552a28a7640865
habui1214 <hnbui511@gmail.com>
2021-07-19 16:42:13 -0400

    added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -4,5 +4,9 @@ public class GitCool {
     // b23 is a special number
     // b23 is making some sense now
 
+    //we have added our github accoutn under intelj
+    //make sure to click apply and click on to save
+
+
 
 }
