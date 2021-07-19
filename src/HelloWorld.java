96967e4c519ddd5509db3837d96a2fe3ed891f23
habui1214 <hnbui511@gmail.com>
2021-07-19 16:20:48 -0400

    added some code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello git World");
+
+        System.out.println("Hello Git");
+        System.out.println("adding awesoeme codes ");
     }
 }
