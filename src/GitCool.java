3df133e666baa8b7ca620fccee82fdc4778f73ca
habui1214 <hnbui511@gmail.com>
2021-07-19 16:44:36 -0400

    added github account 2

--- src/GitCool.java
+++ src/GitCool.java
@@ -7,6 +7,13 @@ public class GitCool {
     //we have added our github accoutn under intelj
     //make sure to click apply and click on to save
 
+    // we were able to successfully shared projects under our github account
+    // share project on github happen only one time per project
+    // it combines the 3 actions below
+    // create remote repo under our account
+    // create the link between local repo to remote repo
+    // push local commits to remote repo
+
 
 
 }
